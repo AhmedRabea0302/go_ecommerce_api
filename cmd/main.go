600381ccfc8db98e6910eb809c8baac8535b2729
AhmedRabea0302 <arabea169@gmail.com>
2024-10-13 21:45:35 +0300

    Added Database Connection

--- cmd/main.go
+++ cmd/main.go
@@ -1,14 +1,44 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 
 	"github.com/AhmedRabea0302/ecom/cmd/api"
+	"github.com/AhmedRabea0302/ecom/config"
+	"github.com/AhmedRabea0302/ecom/db"
+	"github.com/go-sql-driver/mysql"
 )
 
 func main() {
-	server := api.NewAPIServer(":8080", nil)
+	db, err := db.NewNySQLStorage(mysql.Config{
+		User:                 config.Envs.DBUser,
+		Passwd:               config.Envs.DBPassword,
+		Addr:                 config.Envs.DBAddress,
+		DBName:               config.Envs.DBName,
+		Net:                  "tcp",
+		AllowNativePasswords: true,
+		ParseTime:            true,
+	})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	initStorage(db)
+
+	server := api.NewAPIServer(":8080", db)
 	if err := server.Run(); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func initStorage(db *sql.DB) error {
+	err := db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("DB: Successfully Connected!")
+	return nil
 }
